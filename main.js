df93e673719b99412a6e5d70494c8479d70aa79c
Marina8989 <marina.mill209@gmail.com>
2021-09-06 00:16:20 -0700

    js

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const firstNumber = document.querySelector('#firstNumber');
 const secondNumber = document.querySelector('#secondNumber');
-const addBtn = document.querySelector('#addBtn');
-const subtractBtn = document.querySelector('#subtractBtn');
-const multiBtn = document.querySelector('#subtractBtn');
-const divBtn = document.querySelector('#divBtn');
+// const addBtn = document.querySelector('#addBtn');
+// const subtractBtn = document.querySelector('#subtractBtn');
+// const multiBtn = document.querySelector('#subtractBtn');
+// const divBtn = document.querySelector('#divBtn');
+const btns = document.querySelectorAll('.btn');
 const result = document.querySelector('#result');
 const randomNum = document.querySelector('#randomNum');
 const reset = document.querySelector('#reset');
@@ -11,5 +12,17 @@ const submit = document.querySelector('#submit');
 
 submit.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log('click')
+    
 })
+
+function calc() {
+   btns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        console.log(e.target.innerText);
+        if(e.target.innerText === '+') {
+          result.text = Number(firstNumber.innerText) + Number(secondNumber.innerText);
+          console.log(result.innerText)
+        }
+    })
+})
+}
